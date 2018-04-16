ead0448281fb15ac5695410af52b33389227e13f
TuringMacLee <turingmac@hotmail.com>
2018-04-16 16:20:09 -0400

    Print feature map

--- JigsawPuzzleSolver/feature_extractor.py
+++ JigsawPuzzleSolver/feature_extractor.py
@@ -6,11 +6,13 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 from torch.autograd import Variable
 import numpy as np
-from torchvision import datasets, models, transforms
+import torchvision
+from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
 import time
 import os
 import copy
+import resnet_fmap
 
 
 data_transforms = {
@@ -30,11 +32,111 @@ data_transforms = {
 
 data_dir = 'hymenoptera_data'
 image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
-dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=4, shuffle=True, num_workers=4) for x in ['train', 'val']}
+dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1, shuffle=True, num_workers=4) for x in ['train', 'val']}
 dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'val']}
 class_names = image_datasets['train'].classes
 
-model_conv = models.resnet34(pretrained=True)
+
+def imshow(inp, title=None):
+    """Imshow for Tensor."""
+    inp = inp.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+    plt.imshow(inp)
+    if title is not None:
+        plt.title(title)
+    plt.pause(0.001)  # pause a bit so that plots are updated
+
+
+def plot_kernels(tensor, num_cols=8):
+    num_kernels = tensor.shape[0]
+    num_rows = 1+ num_kernels // num_cols
+    fig = plt.figure(figsize=(num_cols,num_rows))
+    i_min = np.min(tensor)
+    i_max = np.max(tensor)
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
+        ax1.imshow(tensor[i])
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
+
+def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
+    since = time.time()
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                scheduler.step()
+                model.train(True)  # Set model to training mode
+            else:
+                model.train(False)  # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            # Iterate over data.
+            for data in dataloaders[phase]:
+                # get the inputs
+                inputs, labels = data
+                out = torchvision.utils.make_grid(inputs)
+                imshow(out)
+
+                # wrap them in Variable
+                inputs = Variable(inputs.cuda())
+                labels = Variable(labels.cuda())
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                #outputs = model(inputs)
+                (layer1_out, layer2_out, layer3_out, layer4_out, outputs) = model(inputs)
+                feature_maps = [layer1_out.data.cpu().numpy()[0], layer2_out.data.cpu().numpy()[0], layer3_out.data.cpu().numpy()[0], layer4_out.data.cpu().numpy()[0]]
+                for fmap in feature_maps:
+                    plot_kernels(fmap, int(np.sqrt(fmap.shape[0])))
+
+                _, preds = torch.max(outputs.data, 1)
+                loss = criterion(outputs, labels)
+
+                # backward + optimize only if in training phase
+                if phase == 'train':
+                    loss.backward()
+                    optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.item() / dataset_sizes[phase]
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
+                phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+        print()
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model
+
+
+model_conv = resnet_fmap.resnet34(pretrained=True)
 for param in model_conv.parameters():
     param.requires_grad = False
 # Parameters of newly constructed modules have requires_grad=True by default
@@ -43,22 +145,18 @@ model_conv.fc = nn.Linear(num_ftrs, 2)
 model_conv = model_conv.cuda()
 criterion = nn.CrossEntropyLoss()
 
-def plot_kernels(tensor, num_cols=8):
-    print(tensor.shape)
-    num_kernels = tensor.shape[0]
-    num_rows = 1+ num_kernels // num_cols
-    fig = plt.figure(figsize=(num_cols,num_rows))
-    i_min = np.min(tensor)
-    i_max = np.max(tensor)
-    for i in range(tensor.shape[0]):
-        ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow((tensor[i] - i_min) / (i_max - i_min))
-        ax1.axis('off')
-        ax1.set_xticklabels([])
-        ax1.set_yticklabels([])
+# Observe that only parameters of final layer are being optimized as
+# opoosed to before.
+optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
 
-    plt.subplots_adjust(wspace=0.1, hspace=0.1)
-    plt.show()
+# Decay LR by a factor of 0.1 every 7 epochs
+exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
+
+model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)
+
+visualize_model(model_conv)
+
+
 
 filters = model_conv.modules()
 body_models = [i for i in model_conv.children()]
