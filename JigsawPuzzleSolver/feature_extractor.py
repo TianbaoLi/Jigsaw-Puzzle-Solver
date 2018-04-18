20300bc34806898e018fe724814e9eb7d1af81aa
TuringMacLee <turingmac@hotmail.com>
2018-04-18 01:43:36 -0400

    Load and crop into 3 * 3 Jigsaw puzzle

--- JigsawPuzzleSolver/feature_extractor.py
+++ JigsawPuzzleSolver/feature_extractor.py
@@ -13,18 +13,13 @@ import time
 import os
 import copy
 import resnet_fmap
-
-
-data_transform = transforms.Compose([
-                transforms.RandomResizedCrop(224),
-                transforms.RandomHorizontalFlip(),
-                transforms.ToTensor(),
-                transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-                ])
+from jigsaw_image_loader import JigsawImageLoader
 
 data_dir = 'ILSVRC2012_img_val/train'
-image_dataset = datasets.ImageFolder(data_dir, data_transform)
+#image_dataset = datasets.ImageFolder(data_dir, data_transform)
+image_dataset = JigsawImageLoader(data_dir)
 dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=4, shuffle=True, num_workers=4)
+
 dataset_size = len(image_dataset)
 #image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
 #dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=1, shuffle=True, num_workers=4) for x in ['train', 'val']}
@@ -45,13 +40,39 @@ def imshow(inp, title=None):
     plt.pause(0.001)  # pause a bit so that plots are updated
 
 
+def back_transform(tensor):
+    inp = tensor.numpy().transpose((1, 2, 0))
+    mean = np.array([0.485, 0.456, 0.406])
+    std = np.array([0.229, 0.224, 0.225])
+    inp = std * inp + mean
+    inp = np.clip(inp, 0, 1)
+
+    return inp
+
+
 def plot_kernels(tensor, num_cols=8):
     num_kernels = tensor.shape[0]
     num_rows = 1+ num_kernels // num_cols
     fig = plt.figure(figsize=(num_cols,num_rows))
     for i in range(tensor.shape[0]):
         ax1 = fig.add_subplot(num_rows,num_cols,i+1)
-        ax1.imshow(tensor[i])
+        ax1.imshow(back_transform(tensor[i]))
+        ax1.axis('off')
+        ax1.set_xticklabels([])
+        ax1.set_yticklabels([])
+
+    plt.subplots_adjust(wspace=0.1, hspace=0.1)
+    plt.show()
+
+
+def plot_jigsaw(tensor):
+    num_tiles = tensor.shape[0]
+    num_cols = int(np.sqrt(num_tiles))
+    num_rows = num_cols
+    fig = plt.figure(figsize=(num_cols, num_rows))
+    for i in range(tensor.shape[0]):
+        ax1 = fig.add_subplot(num_rows, num_cols, i + 1)
+        ax1.imshow(back_transform(tensor[i]))
         ax1.axis('off')
         ax1.set_xticklabels([])
         ax1.set_yticklabels([])
@@ -64,19 +85,25 @@ def gen_feature_map(model):
     model.train(False)
     for data in dataloader:
         # get the inputs
-        inputs, labels = data
-        for input in inputs:
-            input = input[None, :, :]
-            out = torchvision.utils.make_grid(input)
-            imshow(out)
+        jigsaws, orders, tiles = data
+        batch_size = jigsaws.shape[0]
+        for i in range(batch_size):
+            jigsaw = jigsaws[i]
+            order_index = orders[i]
+            plot_jigsaw(jigsaw)
 
-            # wrap them in Variable
-            input = Variable(input.cuda())
-            # get ReLU outputs as the feature maps
-            (layer1_out, output) = model(input)
-            # only use the first block as the low level info
-            first_map = layer1_out.data.cpu().numpy()[0]
-            plot_kernels(first_map, int(np.sqrt(first_map.shape[0])))
+            for tile in jigsaw:
+                input = tile[None, :, :]
+                out = torchvision.utils.make_grid(input)
+                imshow(out)
+
+                # wrap them in Variable
+                input = Variable(input.cuda())
+                # get ReLU outputs as the feature maps
+                (layer1_out, output) = model(input)
+                # only use the first block as the low level info
+                first_map = layer1_out.data.cpu().numpy()[0]
+                plot_kernels(first_map, int(np.sqrt(first_map.shape[0])))
 
 
 def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
---
+++ JigsawPuzzleSolver/jigsaw_image_loader.py
@@ -0,0 +1,74 @@
+import numpy as np
+import torchvision.transforms as transforms
+import torch
+from torchvision.datasets.folder import ImageFolder, default_loader, IMG_EXTENSIONS
+
+import matplotlib.pyplot as plt
+from PIL import Image
+
+
+class JigsawImageLoader(ImageFolder):
+    def __init__(self, root):
+        self.data_path = root
+        self.permutations = self.__retrive_permutations(1000)
+        self.__image_transformer = transforms.Compose([transforms.RandomResizedCrop(224),
+                                                        transforms.RandomHorizontalFlip(),
+                                                        transforms.ToTensor(),
+                                                        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+                                                        ])
+        self.__augment_tile = transforms.Compose([
+                    transforms.RandomCrop(56),
+                    #transforms.Resize((75,75),Image.BILINEAR),
+                    transforms.Lambda(rgb_jittering),
+                    transforms.ToTensor(),
+                    #transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                         #std =[0.229, 0.224, 0.225])
+                                         ])
+        super(ImageFolder, self).__init__(root, default_loader, IMG_EXTENSIONS)
+
+    def __getitem__(self, index):
+        img, target = ImageFolder.__getitem__(self, index)
+
+        #if np.random.rand()<0.30:
+        #    img = img.convert('LA').convert('RGB')
+
+        s = float(img.size[0])/3
+        a = s/2
+        tiles = [None] * 9
+        for n in range(9):
+            i = n/3
+            j = n%3
+            c = [a*i*2+a,a*j*2+a]
+            c = np.array([c[1]-a,c[0]-a,c[1]+a+1,c[0]+a+1]).astype(int)
+            tile = img.crop(c.tolist())
+            tile = self.__augment_tile(tile)
+            # Normalize the patches indipendently to avoid low level features shortcut
+            m, s = tile.view(3,-1).mean(dim=1).numpy(), tile.view(3,-1).std(dim=1).numpy()
+            s[s==0]=1
+            norm = transforms.Normalize(mean=m.tolist(),std=s.tolist())
+            tile = norm(tile)
+            tiles[n] = tile
+        
+        order = np.random.randint(len(self.permutations))
+        data = [tiles[self.permutations[order][t]] for t in range(9)]
+        data = torch.stack(data,0)
+        
+        return data, int(order), tiles
+
+
+    def __retrive_permutations(self, classes):
+        all_perm = np.load('permutations_%d.npy' % (classes))
+        # from range [1,9] to [0,8]
+        if all_perm.min() == 1:
+            all_perm = all_perm - 1
+
+        return all_perm
+
+
+def rgb_jittering(im):
+    im = np.array(im, 'int32')
+    for ch in range(3):
+        im[:,:,ch] += np.random.randint(-2,2)
+    im[im>255] = 255
+    im[im<0] = 0
+    return im.astype('uint8')
