205e122c42b010f2cf4ed46434c851c981f04b9c
TuringMacLee <turingmac@hotmail.com>
2018-04-19 11:51:30 -0400

    Regular 9*4*(224*64*2) feature map for an origin pivture (3*3 tiles)

--- JigsawPuzzleSolver/feature_extractor.py
+++ JigsawPuzzleSolver/feature_extractor.py
@@ -17,8 +17,8 @@ from jigsaw_image_loader import JigsawImageLoader
 
 data_dir = 'ILSVRC2012_img_val/train'
 #image_dataset = datasets.ImageFolder(data_dir, data_transform)
-image_dataset = JigsawImageLoader(data_dir, slice=8)
-dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=1, shuffle=True, num_workers=1)
+image_dataset = JigsawImageLoader(data_dir, slice=3)
+dataloader = torch.utils.data.DataLoader(image_dataset, batch_size=4, shuffle=True, num_workers=4)
 
 dataset_size = len(image_dataset)
 #image_datasets = {x: datasets.ImageFolder(os.path.join(data_dir, x), data_transforms[x]) for x in ['train', 'val']}
@@ -89,8 +89,17 @@ def gen_feature_map(model):
             imshow(origin)
             plot_jigsaw(jigsaw)
             #plot_jigsaw(tile)
+            slice_length = tile[0].shape[1]
 
-            for tile in jigsaw:
+            # store the edge feature map of all tiles
+            # dim1: index of tiles
+            # dim2: direction: up, down, left, right
+            # dim3: tile length * 64 (ReLU1 output maps) * 2 (use 2 rows near the edge)
+            fmp = torch.zeros(9, 4, slice_length * 64 * 2)
+
+            for i in range(jigsaw.shape[0]):
+                tile = jigsaw[i]
+                index = order_index[i]
                 input = tile[None, :, :]
                 out = torchvision.utils.make_grid(input)
                 imshow(out)
@@ -104,94 +113,6 @@ def gen_feature_map(model):
                 plot_kernels(first_map, int(np.sqrt(first_map.shape[0])))
 
 
-def train_model(model, criterion, optimizer, scheduler, num_epochs=25):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train', 'val']:
-            if phase == 'train':
-                scheduler.step()
-                model.train(True)  # Set model to training mode
-            else:
-                model.train(False)  # Set model to evaluate mode
-
-            running_loss = 0.0
-            running_corrects = 0
-
-            # Iterate over data.
-            for data in dataloaders[phase]:
-                # get the inputs
-                inputs, labels = data
-                out = torchvision.utils.make_grid(inputs)
-                imshow(out)
-
-                # wrap them in Variable
-                inputs = Variable(inputs.cuda())
-                labels = Variable(labels.cuda())
-
-                # zero the parameter gradients
-                optimizer.zero_grad()
-
-                # forward
-                # get ReLU outputs as the feature maps
-                (layer1_out, outputs) = model(inputs)
-                # only use the first block as the low level info
-                first_map = layer1_out.data.cpu().numpy()[0]
-                plot_kernels(first_map, int(np.sqrt(first_map.shape[0])))
-
-                _, preds = torch.max(outputs.data, 1)
-                loss = criterion(outputs, labels)
-
-
-                # backward + optimize only if in training phase
-                if phase == 'train':
-                    loss.backward()
-                    optimizer.step()
-
-                # statistics
-                running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.item() / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # deep copy the model
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-        print()
-    # load best model weights
-    model.load_state_dict(best_model_wts)
-    return model
-
-
 model_resnet34 = resnet_fmap.resnet34(pretrained=True)
 model_resnet34 = model_resnet34.cuda()
 gen_feature_map(model_resnet34)
-
-for param in model_conv.parameters():
-    param.requires_grad = False
-# Parameters of newly constructed modules have requires_grad=True by default
-num_ftrs = model_conv.fc.in_features
-model_conv.fc = nn.Linear(num_ftrs, len(class_names))
-model_conv = model_conv.cuda()
-criterion = nn.CrossEntropyLoss()
-
-# Observe that only parameters of final layer are being optimized as
-# opoosed to before.
-optimizer_conv = optim.SGD(model_conv.fc.parameters(), lr=0.001, momentum=0.9)
-
-# Decay LR by a factor of 0.1 every 7 epochs
-exp_lr_scheduler = lr_scheduler.StepLR(optimizer_conv, step_size=7, gamma=0.1)
-
-model_conv = train_model(model_conv, criterion, optimizer_conv, exp_lr_scheduler, num_epochs=25)
